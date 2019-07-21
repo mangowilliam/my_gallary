bc05a7527fe5d326b41819e1c37844a4e34c438a
williammango <williammango2015@gmail.com>
2019-07-21 12:07:54 +0300

    add tests

--- mygallary/models.py
+++ mygallary/models.py
@@ -1,6 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Location(models.Model):
+    locale = models.CharField(max_length = 60)
+    
+    def __str__(self):
+        return self.locale
+    class Meta:
+            ordering = ['locale']
+    
+    def save_location(self):
+        self.save()
+    
+class Category(models.Model):
+    variety = models.CharField(max_length = 60)
+    
+    
+    def __str__(self):
+        return self.variety
+    class Meta:
+            ordering = ['variety']
+    
+    def save_category(self):
+        self.save()
+        
 class Image(models.Model):
     image = models.ImageField(upload_to = 'images/')
     name = models.CharField(max_length = 60)
@@ -14,23 +39,4 @@ class Image(models.Model):
         ordering = ['name']
         
     def save_image(self):
-        self.save()
-
-class Location(models.Model):
-    locale = models.CharField(max_length = 60)
-    
-    def __str__(self):
-        return self.locale
-    
-    def save_location(self):
-        self.save()
-    
-class Category(models.Model):
-    variety = models.CharField(max_length = 60)
-    
-    
-    def __str__(self):
-        return self.variety
-    
-    def save_category(self):
         self.save()
--- mygallary/tests.py
+++ mygallary/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import Location, Category, Image
+
 
 # Create your tests here.
+class LocationTestclass(TestCase):
+    def setUp(self):
+        
