919c2da591887e79cda1ee9e930425281430d8db
williammango <williammango2015@gmail.com>
2019-07-21 09:50:22 +0300

    add models and update settings and urls

--- mygallary/models.py
+++ mygallary/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'images/')
+    name = models.CharField(max_length =60)
+    description = models.TextField()
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+    
+    def __str__(self):
+        return self.name
--- mygallary/urls.py
+++ mygallary/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
 
@@ -6,3 +8,5 @@ from . import views
 urlpatterns = [
     url('^$', views.gallary,name = 'gallary'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
