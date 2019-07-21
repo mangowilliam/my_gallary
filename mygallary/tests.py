a68ffabffb9bfff7aabddcd6ec67ee43fbaf9155
williammango <williammango2015@gmail.com>
2019-07-21 12:48:09 +0300

    uppdate tests

--- mygallary/tests.py
+++ mygallary/tests.py
@@ -5,4 +5,24 @@ from .models import Location, Category, Image
 # Create your tests here.
 class LocationTestclass(TestCase):
     def setUp(self):
+        self.kiambu = Location(locale = 'kiambu')
         
+    def test_instance(self):
+        self.assertTrue(isinstance(self.kiambu,Location))
+        
+    def test_save(self):
+        self.kiambu.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)>0)
+
+class CategoryTestclass(TestCase):
+    def setUp(self):
+        self.food = Category(variety = 'food')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.food,Category))
+    
+    def test_save(self):
+        self.food.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories)>0)
