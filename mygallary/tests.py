6d70647873bac42100c04f4f9f0b121e7e5c0802
williammango <williammango2015@gmail.com>
2019-07-21 13:32:59 +0300

    add image model tests

--- mygallary/tests.py
+++ mygallary/tests.py
@@ -26,3 +26,21 @@ class CategoryTestclass(TestCase):
         self.food.save_category()
         categories = Category.objects.all()
         self.assertTrue(len(categories)>0)
+    
+class ImageTestclass(TestCase):
+    def setUp(self):
+        self.kiambu = Location(locale = 'kiambu')
+        self.kiambu.save_location()
+        
+        self.food = Category(variety = 'food')
+        self.food.save_category()
+        
+        self.new_image = Image(name = 'fufu',description = 'juju inside',location = self.kiambu,cetegory = self.food)
+        self.new_image.save()
+        
+    def tearDown(self):
+        Location.objects.all().delete()
+        Category.objects.all().delete()
+        Image.objects.all().delete()
+        
+        
--- mygallary/urls.py
+++ mygallary/urls.py
@@ -10,3 +10,18 @@ urlpatterns = [
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    
+    
+    
+    
+    
+    
+    
+    
+    #self.fufu= Image(name = 'fufu',description = 'juju inside',location = self.kiambu,cetegory = self.food)
+      #  def test_instance(self):
+            #self.assertTrue(isinstance(self.fufu,Image))
+       # def test_save(self):
+        #    self.fufu.save_image()
+         #   images = Image.objects.all()
+          #  self.assertTrue(len(images)>0)
